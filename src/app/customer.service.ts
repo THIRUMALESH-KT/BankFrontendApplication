79d1e0c3be3123c7833a8b037cfa4f948dbc88d7
THIRUMALESH-KT <tthirumal24@gmail.com>
2023-11-19 18:07:29 +0530

    Your commit message here

--- src/app/customer.service.ts
+++ src/app/customer.service.ts
@@ -9,7 +9,7 @@ import { Address } from './address';
 })
 export class CustomerService {
 
-  private basePath:string="https://54.81.227.186:8080/";
+  private basePath:string="https://54.81.227.186:8081/";
   constructor(private http:HttpClient) { }
 
   saveCustomer(customer:Customer,address:Address):Observable<any>{
